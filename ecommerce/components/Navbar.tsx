0a62b2d99aca3dfa9e585ec85401030adfcb0a0e
Ayanaw Mengesha <ayanawmengesha21@gmail.com>
2025-08-09 09:07:10 -0700

    feat: add navbar

---
+++ ecommerce/components/Navbar.tsx
@@ -0,0 +1,25 @@
+import Link from "next/link";
+
+const Navbar = () => {
+	return (
+		<nav className="sticky top-0 z-50 bg-white shadow">
+			<div className="container mx-auto flex items-center justify-between px-4 py-4">
+				<Link href={"/"} className="hover:text-blue-600">
+					My ECommerce
+				</Link>
+				<div className="hidden md:flex space-x-6">
+					<Link href={"/"}>Home</Link>
+					<Link href={"/products"} className="hover:text-blue-600">
+						Products
+					</Link>
+					<Link href={"/checkout"} className="hover:text-blue-600">
+						Checkout
+					</Link>
+				</div>
+				<div>Hello</div>
+			</div>
+		</nav>
+	);
+};
+
+export default Navbar;
