1e96e97c59656d496f245682997bcbf324b6e86a
Ayanaw Mengesha <ayanawmengesha21@gmail.com>
2025-08-12 00:46:44 -0700

    feat: implement authentication with clerk

--- trello-clone-project/app/page.tsx
+++ trello-clone-project/app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="min-h-screen bg-gradient-to-br from-blue-50 via-white  to-purple-50">
+    <div className="min-h-screen  bg-gradient-to-br from-blue-50 via-white  to-purple-50">
       <Navbar/>
     </div>
   );
--- trello-clone-project/components/navbar.tsx
+++ trello-clone-project/components/navbar.tsx
@@ -1,17 +1,57 @@
-import { Trello } from 'lucide-react'
-import React from 'react'
+'use client'
+import { SignIn, SignInButton, SignUpButton, useUser } from "@clerk/nextjs";
+import { ArrowRight, Trello } from "lucide-react";
+import React from "react";
+import { Button } from "./ui/button";
+import Link from "next/link";
 
 const Navbar = () => {
-  return (
-    <header className='border-b bg-white/80 backdrop-blur-sm sticky top-0 z-50'>
-        <div  className='container  mx-auto px-4 py-3 sm:py-4 flex items-center justify-between'>
-            <div className='flex items-center space-x-2'>
-                <Trello className='h-6 w-6 sm:w-8 sm:h-8 text-blue-600'/>
-                <span className='text-xl sm:text-2xl font-bold text-gray-900'>Trello Clone</span>
-            </div>
-        </div>
-    </header>
-  )
-}
+	const { isSignedIn, user } = useUser();
+    console.log(user)
+	return (
+		<header className="border-b bg-white/80 backdrop-blur-sm sticky top-0 z-50">
+			<div className="container   mx-auto px-4 py-3 sm:py-4 flex items-center justify-between">
+				<div className="flex items-center space-x-2">
+					<Trello className="h-6 w-6 sm:w-8 sm:h-8 text-blue-600" />
+					<span className="text-xl sm:text-2xl font-bold text-gray-900">
+						Trello Clone
+					</span>
+				</div>
+				<div className="flex items-center space-x-2 sm:space-x-4">
+					<div>
+						{isSignedIn ? (
+							<div className="flex flex-col sm:flex-row items-end sm:items-center space-y-1 sm:space-y-0 sm:space-x-4">
+                                <span className="text-xs sm:text-sm text-gray-600 hidden sm:block">Welcome, {user.firstName ?? user.emailAddresses[0].emailAddress} </span>
+                                <Link href={'/dashboard'}>
+                                    <Button size={'sm'} className="text-xm sm:test-sm">Go to Dashboard <ArrowRight/> </Button>
+                                </Link>
+                            </div>
+						) : (
+							<div>
+								<SignInButton>
+									<Button
+										className="text-xm sm:text-sm"
+										variant={"ghost"}
+										size={"sm"}
+									>
+										Sign In
+									</Button>
+								</SignInButton>
+								<SignUpButton>
+									<Button
+										size={"sm"}
+										className="text-xs sm:text-sm"
+									>
+										Sign Up
+									</Button>
+								</SignUpButton>
+							</div>
+						)}
+					</div>
+				</div>
+			</div>
+		</header>
+	);
+};
 
-export default Navbar
+export default Navbar;
